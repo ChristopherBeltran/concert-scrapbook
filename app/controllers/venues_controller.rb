2b323c167808d1ae3ade1983ea77b03ca0d987e6
Christopher Beltran <christod.beltran@gmail.com>
2018-12-19 00:48:33 -0800

    Fix for bug in venues controller that allowed a user to access a myvenues/:slug view for a venue that they have never visited

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -18,19 +18,21 @@ class VenuesController < ApplicationController
 
   get '/myvenues/:slug' do
     if logged_in?
-  @venue = Venue.find_by_slug(params[:slug])
-  @user = current_user
-  @concerts = @user.concerts
-  @venue_concerts = []
-  @concerts.each do |concert|
-    if concert.venue == @venue
-      @venue_concerts << concert
+      @venue = Venue.find_by_slug(params[:slug])
+      @user = current_user
+      @concerts = @user.concerts
+      @venue_concerts = []
+      @concerts.each do |concert|
+        if concert.venue == @venue
+          @venue_concerts << concert
+        end
+      end
+      if @venue_concerts.empty?
+      redirect '/login'
+    else
+      erb :"/venues/show"
     end
   end
-  erb :"/venues/show"
-else
-  redirect '/login'
-end
 end 
 
 
